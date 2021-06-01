8ba2b2e37563fbf6c5ebe9ce92957ee6a2792eb6
UC1309 <arxliu@gmail.com>
2021-05-31 23:51:04 -0500

    commented R code

--- mc_pltr.R
+++ mc_pltr.R
@@ -4,6 +4,12 @@ setwd(dirname(getActiveDocumentContext()$path))
 getwd()
 mc_ap = rep(0, 100)
 mc_ap_train = rep(0, 100)
+
+# START OF MONTE CARLO PLTR LOOP
+# NOTE: SAMPLES 1, 50, AND 79 WILL NOT CONVERGE DUE TO 
+# NUMERICAL INSTABILITY
+# YOU WILL NEED TO ADJUST THE FOR LOOP FROM INTERVALS
+# 2:100, 51:100, AND 80:100
 for (j in 80:100){
   start_time <- Sys.time()
   creditCard <- read.csv(file = glue("mc_pltr_thresh/simul_{j}_thresh.csv"))
@@ -25,12 +31,13 @@ for (j in 80:100){
   Y_matrix = as.matrix(Y)
   X_sparse = Matrix(X_final, sparse=T)
   
-  
+  # 5X2-FOLD DATA SPLIT
   smp_size <- floor(0.5 * nrow(Y_matrix))
   set.seed(12177679)
   ap_scores = rep(0, 10)
   ap_scores_train = rep(0, 10)
-  # for loop starts here
+  
+  # 5 ITERATIONS START HERE
   for (i in 1:5) {
     train_idx <- sample(seq_len(nrow(Y_matrix)),
                         size= smp_size)
@@ -39,18 +46,6 @@ for (j in 80:100){
     Y_train = Y_matrix[train_idx,]
     X_test = X_sparse[-train_idx,]
     Y_test = Y_matrix[-train_idx,]
-    
-    # start_time <- Sys.time()
-    # ridge1 = glmnet(x = X_train, y = Y_train,
-    #                 nlambda = 50,
-    #                 type.measure="class",
-    #                 alpha=0,
-    #                 family="binomial",
-    #                 standardize = F,
-    #                 trace.it=T)
-    # end_time <- Sys.time()
-    # end_time - start_time
-    # plot(ridge1, xvar = "lambda")
     
     # Ridge regression
     start_time <- Sys.time()
@@ -68,22 +63,7 @@ for (j in 80:100){
     best_ridge_coef <- as.numeric(coef(ridge1_cv, 
                                        s = ridge1_cv$lambda.min))[-1]
     
-    # start_time <- Sys.time()
-    # alasso1 = glmnet(x=X_train,
-    #                  y=Y_train,
-    #                  type.measure="class",
-    #                  alpha = 1,
-    #                  maxit = 2e+05,
-    #                  thresh= 1e-06,
-    #                  family="binomial",
-    #                  standardize=F,
-    #                  type.logistic = "modified.Newton",
-    #                  penalty.factor = 1 / abs(best_ridge_coef),
-    #                  trace.it=T)
-    # end_time <- Sys.time()
-    # end_time - start_time
-    # plot(alasso1, xvar = "lambda", label=T)
-    
+    # LASSO regression
     start_time <- Sys.time()
     alasso1_cv <- cv.glmnet(x=X_train,
                             y=Y_train,
@@ -101,12 +81,12 @@ for (j in 80:100){
     end_time <- Sys.time()
     end_time - start_time
     
-    #alasso1_cv$lambda.min
-    # This needs to be saved
+    # Saves LASSO coefficients
     best_alasso_coef1 <- coef(alasso1_cv, s = alasso1_cv$lambda.min)
     writeMM(best_alasso_coef1, 
             file=glue("pltr_coeffs/{j}alasso_coef1_{i}.txt"))
     
+    # Prediction and AP score calculation
     Y_pred = predict(alasso1_cv, 
                      newx = X_test, 
                      type = "response", 
@@ -118,6 +98,7 @@ for (j in 80:100){
     ap_score = pr$auc.integral
     print(ap_score)
     ap_scores[(i*2)-1] = ap_score
+    # Uncomment to see PR curve
     #plot(pr, color="black")
     
     Y_pred_train = predict(alasso1_cv, 
@@ -161,9 +142,7 @@ for (j in 80:100){
                             trace.it=T)
     end_time <- Sys.time()
     end_time - start_time
-    
-    #alasso1_cv$lambda.min
-    # This needs to be saved
+  
     best_alasso_coef2 <- coef(alasso2_cv, s = alasso2_cv$lambda.min)
     writeMM(best_alasso_coef2, 
             file=glue("pltr_coeffs/{j}alasso_coef2_{i}.txt"))
@@ -193,3 +172,5 @@ for (j in 80:100){
   mc_ap[j] = mean(ap_scores)
   mc_ap_train[j] = mean(ap_scores_train)
 }
+mean(mc_ap[mc_ap != 0])
+mean(mc_ap_train[mc_ap != 0])
